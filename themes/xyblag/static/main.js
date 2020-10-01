fff47933906334daee37c68a836d874e8a005743
xypnox <xypnox@gmail.com>
2020-10-01 22:30:40 +0530

    Add smooth theme change

--- themes/xyblag/static/main.js
+++ themes/xyblag/static/main.js
@@ -83,6 +83,7 @@ function initMobile() {
 function toggleTheme() {
   let body = document.getElementsByTagName('body')[0];
   if (body) {
+    body.style.transition = 'background-color 1s ease-in-out';
     if (body.getAttribute('data-theme') === 'light') {
       body.setAttribute('data-theme', 'dark');
       window.localStorage.setItem('theme', 'dark');
