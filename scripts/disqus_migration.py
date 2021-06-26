9767afaa79302db13d8c5170e99eee282431b696
xypnox <xypnox@gmail.com>
2021-06-26 16:08:19 +0530

    Refactor and comment the script for disqus migration

--- scripts/disqus_migration.py
+++ scripts/disqus_migration.py
@@ -1,78 +1,66 @@
 import os
-import time
 import json
-import pprint
 import xmltodict
-from github import Github
 
 FILEPATH = "comments.xml"
 
-pp = pprint.PrettyPrinter(indent=4)
 
-
-def disqus_to_github():
+def disqus_to_markdown():
     with open(FILEPATH) as fd:
         data = xmltodict.parse(fd.read())
 
     data = data["disqus"]
 
     threads = [dict(t) for t in data["thread"]]
-    # pp.pprint(threads)
     posts = sorted((dict(p)
                    for p in data["post"]), key=lambda d: d["createdAt"])
 
     # only keep threads with comments
     twc_ids = set(p["thread"]["@dsq:id"] for p in posts)
     threads = {t["@dsq:id"]: t for t in threads if t["@dsq:id"] in twc_ids}
-    # pp.pprint(threads)
 
     titles = {}
+    # We get the post metadata and the link for the title
+    # We index it with @dsq:id as comments are also associated with it
+    # You can change the f string in message for a different markdown output
     for key in threads:
-        # pp.pprint(dict(key['thread'])['@dsq:id'])
-        # pp.pprint(key)
-        # exit()
         title = threads[key]["@dsq:id"]
-        message = f"## [{threads[key]['title']}]({threads[key]['link']})"
+        [title_string, post_link] = [threads[key]['title'], threads[key]['link']]
+
+        message = f"## [{title_string}]({post_link})"
+
         if title not in titles:
             titles[title] = message
         else:
             titles[title] = message
 
-    # pp.pprint(titles)
     # exit()
 
-    # associate the related GitHub issue to each thread
-    # warning: the issues need to exist before you run this script!
-    # write a "test" comment in each one of your post with comments
-    # to make Utterances create the initial issues
-    links = {}
-    # associate the thread to each post
+    comments = {}
+    # associate the comments to each post's dsq:id
     for post in posts:
-        # pp.pprint(dict(post['thread'])['@dsq:id'])
-        # pp.pprint(post)
-        # exit()
-        link = dict(post['thread'])['@dsq:id']
+        dsq_id = dict(post['thread'])['@dsq:id']
+
         if post["message"]:
             message = "#### **" + dict(post['author'])['name'] + \
                 "** @" + dict(post['author'])['username'] + \
                 "\n\nPosted on: _" + post['createdAt'] + "_ " + \
                 "\n\n" + post["message"] + "\n"
-            if link not in links:
-                links[link] = [message]
+
+            if dsq_id not in comments:
+                comments[dsq_id] = [message]
             else:
-                links[link].append(message)
+                comments[dsq_id].append(message)
 
-        # pp.pprint(post["@dsq:id"])
-        # post["thread"] = threads[post["thread"]["@dsq:id"]]
-
-    pp.pprint(links)
-    # pp.pprint(threads)
-    for key in links:
-
+    for key in comments:
+        # Print the comments in markdown
         print(f"\n\n---\n\n{titles[key]} \n")
-        msg = "\n".join(links[key])
+
+        # Join all comments for a post
+        msg = "\n".join(comments[key])
+
         print(msg)
 
 
 if __name__ == "__main__":
-    disqus_to_github()
+    disqus_to_markdown()
